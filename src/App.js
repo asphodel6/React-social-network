066673b033d58ed399aced5ae70e6432b7ee3b17
asphodel6 <harmsss@mail.ru>
2023-03-20 20:33:14 +0500

    Lesson 38

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = (props) => {
         <div className="app-wrapper-content">
           <Routes>
             <Route path="/dialogs" element={<Dialogs state={props.state.dialogsPage}/>} />
-            <Route path="/profile" element={<Profile profilePage={props.state.profilePage} addPost={props.addPost} updateNewPostText={props.updateNewPostText} />} />
+            <Route path="/profile" element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />} />
           </Routes>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = () => {
   root.render(
     <React.StrictMode>
-      <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
+      <App state={store.getState()} dispatch={store.dispatch.bind(store)}  />
     </React.StrictMode>
   );
 };
