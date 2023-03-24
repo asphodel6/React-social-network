fd3f654b896bf9ccffbe555556ebfe2b77fce3c9
asphodel6 <harmsss@mail.ru>
2023-03-24 20:55:09 +0500

    47 profile state

--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import store from './redux/redux-store';
-import { Provider } from './StoreContext';
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import store from "./redux/redux-store";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-let rerenderEntireTree = () => {
-  root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-      <App state={store.getState()} dispatch={store.dispatch.bind(store)} store={store} />
-      </Provider>
-    </React.StrictMode>
-  );
-};
-
-
-rerenderEntireTree(store.getState());
-
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
-});
